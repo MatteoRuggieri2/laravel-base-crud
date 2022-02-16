a35a5159531c32b4afb4b46bb55e35c4bfe963d8
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-02-16 17:40:28 +0100

    Creato route singolo comic

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,23 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('page_title')
+    Comics
+@endsection
+
+@section('main_content')
     
-    <main>
+    <h1>Lista di fumetti</h1>
+            
+    @foreach ($comics as $comic)
+        <h3><a href="#">{{ $comic->title }}</a></h3>
+        <div>Serie: {{ $comic->series }}</div>
+    @endforeach
 
-        <h1>Lista di fumetti</h1>
-        
-        @foreach ($comics as $comic)
-            <h3><a href="#">{{ $comic->title }}</a></h3>
-            <div>Serie: {{ $comic->series }}</div>
-        @endforeach
-
-    </main>
-
-</body>
-</html>
+@endsection
---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('page_title')
+    Show Comic
+@endsection
+
+@section('main_content')
+    
+    <h1>titolo singolo fumetto</h1>
+
+@endsection
